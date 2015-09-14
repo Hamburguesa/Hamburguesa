c88bd93ab473b9e1bc61a15a46a2498143cbab07
Manuel <manuelfreire98@gmail.com>
2015-09-14 18:24:02 -0300

    Primer menu agregado

--- Hamburguesa/Hamburguesa/principal.py
+++ Hamburguesa/Hamburguesa/principal.py
@@ -1,10 +1,100 @@
 import pygame
 
 import constantes
+from menu import cMenu, EVENT_CHANGE_STATE
+
 from nivel1 import Level_01
 from nivel2 import Level_02 
 
 from jugador import Player
+
+
+def Play(pantalla):
+    # Creamos al jugador con la imagen p1_walk.png
+    jugador_principal = Player("imagenes/hamburguesa.png")
+    letraParaPuntos = pygame.font.Font(None, 24)
+    letraParaVidas = pygame.font.Font(None, 24)
+# Creamos todos los niveles del juego
+    lista_niveles = []
+    lista_niveles.append(Level_01(jugador_principal))
+    lista_niveles.append(Level_02(jugador_principal))
+# Seteamos cual es el primer nivel.
+    numero_del_nivel_actual = 0
+    nivel_actual = lista_niveles[numero_del_nivel_actual]
+    lista_sprites_activos = pygame.sprite.Group()
+    jugador_principal.nivel = nivel_actual
+    jugador_principal.rect.x = 240
+    jugador_principal.rect.y = constantes.LARGO_PANTALLA - jugador_principal.rect.height - 120
+    lista_sprites_activos.add(jugador_principal)
+    letraParaMarcador1 = pygame.font.Font(None, 56)
+    letraParaMarcador2 = pygame.font.Font(None, 36)
+#Variable booleano que nos avisa cuando el usuario aprieta el botOn salir.
+    salir = False
+    clock = pygame.time.Clock()
+# -------- Loop Principal -----------
+    while not salir:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                salir = True
+            if evento.type == pygame.KEYDOWN:
+                if evento.key == pygame.K_LEFT:
+                    jugador_principal.retroceder()
+                if evento.key == pygame.K_RIGHT:
+                    jugador_principal.avanzar()
+                if evento.key == pygame.K_UP:
+                    jugador_principal.saltar()
+                if evento.key == pygame.K_ESCAPE:
+                    salir = True
+            if evento.type == pygame.KEYUP:
+                if evento.key == pygame.K_LEFT and jugador_principal.mover_x < 0:
+                    jugador_principal.parar()
+                if evento.key == pygame.K_RIGHT and jugador_principal.mover_x > 0:
+                    jugador_principal.parar()
+        
+        # Actualiza todo el jugador
+        lista_sprites_activos.update()
+        # Actualiza los elementos del nivel
+        nivel_actual.update()
+        # Si el jugador se acarca hacia el lado derecho mueve el mundo hacia la izquierda (-x)
+        if jugador_principal.rect.x >= 500:
+            diff = jugador_principal.rect.x - 500
+            jugador_principal.rect.x = 500
+            nivel_actual.avance_nivel(-diff)
+        # Si el jugador se acarca hacia el lado izquierda mueve el mundo hacia la derecha (-x)
+        if jugador_principal.rect.x <= 120:
+            diff = 120 - jugador_principal.rect.x
+            jugador_principal.rect.x = 120
+            nivel_actual.avance_nivel(diff)
+        #Si el jugador se mueve hacia el fin del nivel cambia el jugador al siguiente nivel.
+        current_position = jugador_principal.rect.x + nivel_actual.posicion_jugador_nivel
+        if current_position < nivel_actual.limite_nivel:
+            jugador_principal.rect.x = 120
+            if numero_del_nivel_actual < len(lista_niveles) - 1:
+                numero_del_nivel_actual += 1
+                nivel_actual = lista_niveles[numero_del_nivel_actual]
+                jugador_principal.nivel = nivel_actual
+        # TODO EL CODIGO PARA DIBUJAR DEBE IR DEBAJO DE ESTE COMENTARIO.
+        nivel_actual.draw(pantalla)
+        lista_sprites_activos.draw(pantalla)
+        textoPuntos = letraParaPuntos.render("Puntos: " + str(jugador_principal.puntos), 1, constantes.NEGRO)
+        pantalla.blit(textoPuntos, (10, 10))
+        textoVidas = letraParaVidas.render("Vidas: " + str(jugador_principal.vidas), 1, constantes.NEGRO)
+        pantalla.blit(textoVidas, (900, 10))
+        # TODO EL CODIGO PARA DIBUJAR DEBE IR POR ARRIBA DE ESTE COMENTARIO.
+        clock.tick(60)
+        pygame.display.flip()
+        if jugador_principal.vidas == 0:
+            pantalla.fill(constantes.NEGRO)
+            texto_gameover1 = letraParaMarcador1.render("GAME OVER", 1, constantes.ROJO)
+            texto_gameover2 = letraParaMarcador2.render("Presione cualquier tecla para volver a jugar", 1, constantes.ROJO)
+            pantalla.blit(texto_gameover1, [300, 250])
+            pantalla.blit(texto_gameover2, [200, 310])
+            pygame.display.flip()
+            pygame.event.wait()
+            salir = True
+    #Salgo del juego
+    return True
+    
 
 def main():
     """ Clase principal en el que se debe ejecutar el juego. """
@@ -19,124 +109,38 @@ def main():
     sonido3.play(-1)
     pygame.display.set_caption("Comida rapida")
 
-    # Creamos al jugador con la imagen p1_walk.png
-    jugador_principal = Player("imagenes/hamburguesa.png")
-    
-    
-    letraParaPuntos = pygame.font.Font(None,24)
-    letraParaVidas  = pygame.font.Font(None,24)
-    
-    # Creamos todos los niveles del juego
-    lista_niveles = []
-
-    lista_niveles.append(Level_01(jugador_principal))
-    lista_niveles.append(Level_02(jugador_principal))
-
-    # Seteamos cual es el primer nivel.
-    numero_del_nivel_actual = 0
-    nivel_actual = lista_niveles[numero_del_nivel_actual]
-
-    lista_sprites_activos = pygame.sprite.Group()
-    jugador_principal.nivel = nivel_actual
-
-    jugador_principal.rect.x = 240
-    jugador_principal.rect.y = constantes.LARGO_PANTALLA - jugador_principal.rect.height -120
-    lista_sprites_activos.add(jugador_principal)
-    
-    letraParaMarcador1 = pygame.font.Font(None, 56)
-    letraParaMarcador2 = pygame.font.Font(None, 36)
-
-    #Variable booleano que nos avisa cuando el usuario aprieta el botOn salir.
+    menu_principal = cMenu(50,50,20,10,"vertical",3,pantalla, [("Jugar", 1, None),("Creditos", 2, None),("Salir", 3, None)])
+    estado = 0
+    estado_previo = 1
+    opcion = []
     salir = False
-
-    clock = pygame.time.Clock()
-
-    # -------- Loop Principal -----------
-    while not salir:
-        for evento in pygame.event.get(): 
-            if evento.type == pygame.QUIT: 
-                salir = True 
-
-            if evento.type == pygame.KEYDOWN:
-                if evento.key == pygame.K_LEFT:
-                    jugador_principal.retroceder()
-                if evento.key == pygame.K_RIGHT:
-                    jugador_principal.avanzar()
-                if evento.key == pygame.K_UP:
-                    jugador_principal.saltar()
-                if evento.key == pygame.K_ESCAPE:
-                    salir = True
-                
-                    
-
-            if evento.type == pygame.KEYUP:
-                if evento.key == pygame.K_LEFT and jugador_principal.mover_x < 0:
-                    jugador_principal.parar()
-                if evento.key == pygame.K_RIGHT and jugador_principal.mover_x > 0:
-                    jugador_principal.parar()
-
-
-
-
-        # Actualiza todo el jugador
-        lista_sprites_activos.update()
-
-
-        # Actualiza los elementos del nivel
-        nivel_actual.update()
-
-
-        # Si el jugador se acarca hacia el lado derecho mueve el mundo hacia la izquierda (-x)
-        if jugador_principal.rect.x >= 500:
-            diff = jugador_principal.rect.x - 500
-            jugador_principal.rect.x = 500
-            nivel_actual.avance_nivel(-diff)
-
-
-        # Si el jugador se acarca hacia el lado izquierda mueve el mundo hacia la derecha (-x)
-        if jugador_principal.rect.x <= 120:
-            diff = 120 - jugador_principal.rect.x
-            jugador_principal.rect.x = 120
-            nivel_actual.avance_nivel(diff)
-
-        #Si el jugador se mueve hacia el fin del nivel cambia el jugador al siguiente nivel.
-        current_position = jugador_principal.rect.x + nivel_actual.posicion_jugador_nivel
-        if current_position < nivel_actual.limite_nivel:
-            jugador_principal.rect.x = 120
-            if numero_del_nivel_actual < len(lista_niveles)-1:
-                numero_del_nivel_actual += 1
-                nivel_actual = lista_niveles[numero_del_nivel_actual]
-                jugador_principal.nivel = nivel_actual
-
-
-        # TODO EL CODIGO PARA DIBUJAR DEBE IR DEBAJO DE ESTE COMENTARIO.
-        nivel_actual.draw(pantalla)
-        lista_sprites_activos.draw(pantalla)
-        textoPuntos = letraParaPuntos.render("Puntos: "+str(jugador_principal.puntos), 1,constantes.NEGRO)
-        pantalla.blit(textoPuntos,(10,10))
-        textoVidas = letraParaVidas.render("Vidas: "+str(jugador_principal.vidas), 1,constantes.NEGRO)
-        pantalla.blit(textoVidas,(900,10))
+    
+    while (not salir):
+        e = pygame.event.wait()
         
+        if estado != estado_previo:
+            pygame.event.post(pygame.event.Event(EVENT_CHANGE_STATE, key = 0))
+            estado_previo = estado
         
-        # TODO EL CODIGO PARA DIBUJAR DEBE IR POR ARRIBA DE ESTE COMENTARIO.
-
-
-
-
-        clock.tick(60)
-
-        pygame.display.flip()
-
-        if jugador_principal.vidas == 0:
-            pantalla.fill(constantes.NEGRO)
-            texto_gameover1 = letraParaMarcador1.render("GAME OVER", 1, constantes.ROJO)
-            texto_gameover2 = letraParaMarcador2.render("Presione cualquier tecla para volver a jugar", 1, constantes.ROJO)
-            pantalla.blit(texto_gameover1, [300, 250])
-            pantalla.blit(texto_gameover2, [200, 310])
-            pygame.display.flip()
-            pygame.event.wait()
+        if e.type == pygame.KEYDOWN or e.type == EVENT_CHANGE_STATE:
+            if estado == 0:
+                opcion, estado = menu_principal.update(e, estado)
+            if estado == 1:
+                salir = Play(pantalla)
+            if estado == 2:
+                pantalla.fill(constantes.ROJO)
+                opcion, estado = menu_principal.update(e, estado)
+            if estado == 3:
+                salir = True
+        if e.type == pygame.QUIT:
             salir = True
+            
+        pygame.display.update(opcion)
+
+        
     pygame.quit()
+
+    
 
 if __name__ == "__main__":
     main()
